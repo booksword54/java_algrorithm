e848bd50c0666e375a023b58e7da0d6685aac041
九渊 <1058477702@qq.com>
2024-08-16 22:45:49 +0800

    add: 盛最多水的容器

---
+++ Two_Pointers/src/ContainerWithMostWater.java
@@ -0,0 +1,21 @@
+public class ContainerWithMostWater {
+    // 盛最多水的容器
+    // 给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+    // 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+    // 返回容器可以储存的最大水量。
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int res = 0;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                res = Math.max(res, (right - left) * height[left]);
+                left++;
+            } else {
+                res = Math.max(res, (right - left) * height[right]);
+                right--;
+            }
+        }
+        return res;
+    }
+}
