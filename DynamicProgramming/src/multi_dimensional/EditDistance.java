d53d57771972e8e31eb74f83852183b77be62b65
九渊 <1058477702@qq.com>
2024-08-18 02:12:49 +0800

    update: 编辑距离

--- DynamicProgramming/src/multi_dimensional/EditDistance.java
+++ DynamicProgramming/src/multi_dimensional/EditDistance.java
@@ -10,24 +10,24 @@ public class EditDistance {
     public int minDistance(String word1, String word2) {
         int m = word1.length();
         int n = word2.length();
-        int[][] dp = new int[m + 1][n + 1];
-        // 删除word1中所有字符
+        int[][] dp = new int[m + 1][n + 1]; // 表示从m个字符的word1 到n个字符的word2所需的操作数
+        // 删除word1的所有字符，从 i 到 0
         for (int i = 0; i <= m; i++) {
             dp[i][0] = i;
         }
-        // 删除word2中所有字符
+        // 添加word2的所有字符，从 0 到 j
         for (int j = 0; j <= n; j++) {
             dp[0][j] = j;
         }
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
-                int add = dp[i - 1][j] + 1;
-                int delete = dp[i][j - 1] + 1;
-                int replace;
+                int add = dp[i][j - 1] + 1; // 添加字符 word2[j]
+                int delete = dp[i - 1][j] + 1; // 删除字符 word1[i]
+                int replace; // 替换字符
                 if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
                     replace = dp[i - 1][j - 1];
                 } else {
-                    replace = dp[i - 1][j - 1] + 1;
+                    replace = dp[i - 1][j - 1] + 1; // 替换 word1[i] 为 word2[j]
                 }
                 dp[i][j] = Math.min(add, Math.min(delete, replace));
             }
