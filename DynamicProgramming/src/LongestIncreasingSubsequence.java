d4fda4a10f05f658d39e24ccb009e954822d9198
九渊 <1058477702@qq.com>
2024-08-17 22:38:24 +0800

    add: dynamic-programming_最长递增子序列

---
+++ DynamicProgramming/src/LongestIncreasingSubsequence.java
@@ -0,0 +1,33 @@
+public class LongestIncreasingSubsequence {
+    // 最长递增子序列
+    // 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+    //子序列 是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n]; // 递增区间
+        int idx = 0;
+        dp[idx++] = nums[0];
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > dp[idx - 1]) {
+                dp[idx++] = nums[i]; // 可以放到递增区间
+                continue;
+            }
+            // 二分查找，找到第一个大于或等于的元素并替换它
+            // 为什么还替换等于的元素？，因为严格递增序列不能出现重复元素
+            // 只替换大元素可能导致重复，即前面有相同的元素
+            int left = 0;
+            int right = idx - 1;
+            while (left < right) {
+                int mid = ((right - left) >> 1) + left;
+                if (nums[i] <= dp[mid]) {
+                    right = mid; // 相等元素也替换
+                } else {
+                    left = mid + 1;
+                }
+            }
+            dp[right] = nums[i]; // 替换
+        }
+        return idx;
+    }
+
+}
